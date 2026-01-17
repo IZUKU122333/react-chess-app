89cd4b0598698a246c1b8e67b7076325b0ec5c12
IZUKU122333 <abhiycu@gmail.com>
2026-01-17 10:30:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-chess-app/'  // <-- IMPORTANT: use your GitHub repo name
+  base: '/react-chess-app/'
 })
